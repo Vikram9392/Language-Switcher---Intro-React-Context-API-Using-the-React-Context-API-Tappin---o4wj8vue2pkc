771c66633dc4c25a7035a852d4bb4bbfa8bc27cc
Vikram9392 <148332548+Vikram9392@users.noreply.github.com>
2024-03-06 16:12:53 +0530

    Update LanguageContext.js

---
+++ contexts/LanguageContext.js
@@ -0,0 +1,21 @@
+import React, { createContext, useState, useContext } from 'react';
+
+const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState('English');
+
+  const changeLanguage = (selectedLanguage) => {
+    setLanguage(selectedLanguage);
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
+
+export const useLanguage = () => {
+  return useContext(LanguageContext);
+};
